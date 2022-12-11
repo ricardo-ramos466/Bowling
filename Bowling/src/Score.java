cc524ec684c4bc9f76ba36dadbd1c8a89368ca10
Ricardo Ramos <ricardo.r.p466@gmail.com>
2022-12-11 16:11:25 -0500

    frame setup complete at needs review, precessing algorithm for score calculation

--- Bowling/src/Score.java
+++ Bowling/src/Score.java
@@ -6,25 +6,53 @@ public class Score {
 
 
 
-    public void SetAllFrames(List<Integer> plays){
+    public void setAllFrames(List<Integer> plays){
         int frame = 1;
         Map<Integer,List<Integer>> frames = new HashMap<>();
         for( int i = 0; i< plays.size(); i++){
-            if(plays.get(i)==10 && (i != plays.size()-3) || i != plays.size()-2) {
+            if(plays.get(i)==10 && frame !=10) {
                 frames.put(frame, Collections.singletonList(plays.get(i)));
                 frame++;
-            }else if( i != plays.size()-3 || i != plays.size()-2){
+            }else if( frame != 10){
                 List<Integer> scores = new ArrayList<>();
                 scores.add(plays.get(i));
                 scores.add(plays.get(i+1));
                 frames.put(frame,scores);
                 i++;
+                frame++;
+            } else if(frame == 10){
+                List<Integer> scores = new ArrayList<>();
+                scores.add(i);
+                scores.add(i+1);
+                if (plays.size()-i == 3){
+                    scores.add(i+2);
+                }
+                i+=2;
             }
         }
+        setFrames(frames);
 
     }
 
-    public int calculateScore(){
+    public int calculateScore(List<Integer> listOfPlays){
+        setAllFrames(listOfPlays);
+        Map<Integer,List<Integer>> frames = getFrames();
+        int currentFrameScore = 0;
+        int totalScore = 0;
+        for(int i = 1; i<=10; i++){
+            if(i !=10){
+                if(frames.get(i).size() == 2 && frames.get(i).get(0) + frames.get(i).get(1) != 10){
+                    totalScore += frames.get(i).get(0) + frames.get(i).get(1);
+                } else if(frames.get(i).size() == 2 && frames.get(i).get(0) + frames.get(i).get(1) == 10){
+                    currentFrameScore = frames.get(i).get(0) + frames.get(i).get(1) + frames.get(i+1).get(0);
+                    totalScore += currentFrameScore;
+                } else if(frames.get(i).size() == 1){
+                    
+                }
+            }
+
+        }
+
         return 0;
     }
 
